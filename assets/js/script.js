5c46762b405489f635a49d81fdfc01e882c39091
ParkerCF <faul.parker@outlook.com>
2022-11-14 22:21:59 -0500

    added some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,48 @@
-// Assignment code here
+// Arrays of special characters, 
 
+function getRandonmLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+function getRandomSymbol() {
+  var specialCharacters = [
+    '@',
+    '%',
+    '+',
+    '\\',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.',
+  ];
+  
+  return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+}
+
+console.log(getRandomSymbol ());
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
